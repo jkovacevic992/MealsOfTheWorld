d6468993aaa28ea5f92bbb48ade9e1454f2ca882
josip <jkovacevic992@gmail.com>
2023-03-03 16:56:11 +0100

    Removed KSP translation package; Installed GEDMO translation package; Installed fixtures; Entity rework

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Knp\DoctrineBehaviors\DoctrineBehaviorsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
