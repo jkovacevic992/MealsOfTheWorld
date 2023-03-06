1fe7d131820af92ec85a0a5c1138f5213f70d076
josip <jkovacevic992@gmail.com>
2023-03-06 20:39:33 +0100

    New migration and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Installation:
+- configure .env
+- run "composer install"
+- Create database: php bin/console doctrine:database:create
+- Run migrations =
--- migrations/Version20230306193528.php
+++ migrations/Version20230306193528.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230303173157 extends AbstractMigration
+final class Version20230306193528 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,13 @@ final class Version20230303173157 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ext_translations (id INT AUTO_INCREMENT NOT NULL, locale VARCHAR(8) NOT NULL, object_class VARCHAR(191) NOT NULL, field VARCHAR(32) NOT NULL, foreign_key VARCHAR(64) NOT NULL, content LONGTEXT DEFAULT NULL, INDEX translations_lookup_idx (locale, object_class, foreign_key), INDEX general_translations_lookup_idx (object_class, foreign_key), UNIQUE INDEX lookup_unique_idx (locale, object_class, field, foreign_key), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB ROW_FORMAT = DYNAMIC');
         $this->addSql('CREATE TABLE ingredient (id INT AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE meal (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, created_at DATETIME NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_9EF68E9C12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE meals_tags (meal_id INT NOT NULL, tag_id INT NOT NULL, INDEX IDX_F83DC9A6639666D6 (meal_id), INDEX IDX_F83DC9A6BAD26311 (tag_id), PRIMARY KEY(meal_id, tag_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE meals_ingredients (meal_id INT NOT NULL, ingredient_id INT NOT NULL, INDEX IDX_DF77A0AB639666D6 (meal_id), INDEX IDX_DF77A0AB933FE08C (ingredient_id), PRIMARY KEY(meal_id, ingredient_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE tag (id INT AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE meal ADD CONSTRAINT FK_9EF68E9C12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE meals_tags ADD CONSTRAINT FK_F83DC9A6639666D6 FOREIGN KEY (meal_id) REFERENCES meal (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE meals_tags ADD CONSTRAINT FK_F83DC9A6BAD26311 FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE');
@@ -42,10 +44,12 @@ final class Version20230303173157 extends AbstractMigration
         $this->addSql('ALTER TABLE meals_ingredients DROP FOREIGN KEY FK_DF77A0AB639666D6');
         $this->addSql('ALTER TABLE meals_ingredients DROP FOREIGN KEY FK_DF77A0AB933FE08C');
         $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE ext_translations');
         $this->addSql('DROP TABLE ingredient');
         $this->addSql('DROP TABLE meal');
         $this->addSql('DROP TABLE meals_tags');
         $this->addSql('DROP TABLE meals_ingredients');
         $this->addSql('DROP TABLE tag');
+        $this->addSql('DROP TABLE messenger_messages');
     }
 }
